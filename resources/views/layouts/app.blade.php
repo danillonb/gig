b4552278a5067dbe0acc43657cdd10d09a005174
Danillo Barbosa <danillonb2@gmail.com>
2018-05-29 01:21:37 -0300

    Heroku upload test 14

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,11 +13,7 @@
     <title>{{ config('app.name', 'Laravel') }} - {{Route::currentRouteName()}}</title>
 
     <!-- Styles / Fonts / Scripts / Fonts -->
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+    <link rel="stylesheet" charset="utf-8" href="https://fonts.googleapis.com/icon?family=Material+Icons"><link rel="stylesheet" charset="utf-8" href="http://gestao-igrejas.test/materialize-css/css/materialize.min.css"><script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script><script type="text/javascript" src="http://gestao-igrejas.test/materialize-css/js/materialize.min.js"></script>
 </head>
 <body @if(Session::has('message')) onload="M.toast({html: '{{ Session::get('message') }}', classes: 'rounded teal'})" @endif >
 <div id="app">
