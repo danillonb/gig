e6a53a96501293ba364a448d3389227c110ffe63
Danillo Barbosa <danillonb2@gmail.com>
2018-05-29 01:00:17 -0300

    Heroku upload test 6

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,11 @@
     <title>{{ config('app.name', 'Laravel') }} - {{Route::currentRouteName()}}</title>
 
     <!-- Styles / Fonts / Scripts / Fonts -->
-    {!! MaterializeCSS::include_full() !!}
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
 </head>
 <body @if(Session::has('message')) onload="M.toast({html: '{{ Session::get('message') }}', classes: 'rounded teal'})" @endif >
 <div id="app">
