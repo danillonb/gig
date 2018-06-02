e5a38a1e29a83fe43a4499422a1bdba07202eff5
Danillo Barbosa <danillonb2@gmail.com>
2018-06-02 09:27:05 -0300

    merge all triggers in a single init.js file

--- public/materialize-css/js/init.js
+++ public/materialize-css/js/init.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.carousel').carousel();
+    $('.collapsible').collapsible();
+    $(".dropdown-trigger").dropdown();
+    $('.modal').modal();
+    $('.scrollspy').scrollSpy();
+    $('select').formSelect();
+    $('.tooltipped').tooltip();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+        hoverEnabled: false
+    });
 });
